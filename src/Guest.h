18aa2b72363cdf7dec2318de1a2de00bc79cd44a
TamaraS93 <tamarica.stanic93@gmail.com>
2020-02-08 19:59:40 +0100

    Added function for print with JMBG

--- src/Guest.h
+++ src/Guest.h
@@ -2,19 +2,18 @@
 
 class Guest : public Person
 {
-private:
+public:
     int room_;
     int number_of_reservtion_;
 
-public:
+    //public:
     Guest(int room = 0, int number_of_reservation = 0) : room_(room),
                                                          number_of_reservtion_(number_of_reservation)
     {
     }
-    //  Guest *function_insert_guest() { return nullptr; }
-    void function_print_guest_with_JMBG() {}
+    void function_insert_guest();
+    void function_print_guest_with_JMBG();
     void function_print_guests() {}
     void function_delete_guest() {}
     void clear_guests() {}
-    // Person *function_insert_person(long long int *function_JMBG) { return nullptr; }
 };
--- src/List.cpp
+++ src/List.cpp
@@ -1,7 +1,9 @@
 #include "List.h"
 #include "Person.h"
+#include "Guest.h"
 
 Person *person_head = nullptr;
+Guest *guest_head = nullptr;
 
 void List::insert(Person *person)
 {
@@ -30,6 +32,34 @@ Person *List::check_for_person(long long int JMBG_function)
         }
         current = current->next;
     }
-
     return nullptr;
+}
+void List::print_with_JMBG(long long int function_JMBG)
+{
+    Guest *current = guest_head;
+
+    if (guest_head == nullptr)
+    {
+        std::cout << "Guest list is empty" << std::endl;
+    }
+    else
+    {
+        while (current)
+        {
+            if (current->JMBG_ == function_JMBG)
+            {
+                std::cout << "JMBG:" << current->JMBG_;
+                std::cout << "\n";
+                std::cout << "Name:" << current->name_;
+                std::cout << "\n";
+                std::cout << "Address:" << current->address_;
+                std::cout << "\n";
+                std::cout << "Room:" << current->room_;
+                std::cout << "\n";
+                std::cout << "Number of reservation:" << current->number_of_reservtion_;
+                std::cout << "\n";
+                std::cout << "**********" << std::endl;
+            }
+        }
+    }
 }
